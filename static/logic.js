792b1c42e5aae8bc2a7047295171bb5b6e6ff6d3
Tstridiron <64445568+Tstridiron@users.noreply.github.com>
2020-09-25 00:45:53 -0400

    Add files via upload

--- static/logic.js
+++ static/logic.js
@@ -1,15 +1,45 @@
 // Read the json file in for pitching table
 d3.json('/static/pitching.json').then(function(users) {
-    var data = {
-        Years: users.map(function(user) {
+    var dataERA = {
+        labels: users.map(function(user) {
             return user.Year;
         }),
-        ERA: users.map(function(user) {
+        series: users.map(function(user) {
             return user.ERA;
         })
     };
     console.log(data)
-    var chart = new Chartist.Bar('#chart', data, {
+    var chart = new Chartist.Bar('#chart', dataERA, {
+        distributeSeries: true
+    });
+});
+
+d3.json('/static/pitching.json').then(function(users) {
+    var dataHR = {
+        labels: users.map(function(user) {
+            return user.Year;
+        }),
+        series: users.map(function(user) {
+            return user.HR;
+        })
+    };
+    console.log(data)
+    var chart = new Chartist.Bar('#chart', dataHR, {
+        distributeSeries: true
+    });
+});
+
+d3.json('/static/pitching.json').then(function(users) {
+    var dataSO = {
+        labels: users.map(function(user) {
+            return user.Year;
+        }),
+        series: users.map(function(user) {
+            return user.HR;
+        })
+    };
+    console.log(data)
+    var chart = new Chartist.Bar('#chart', dataSO, {
         distributeSeries: true
     });
 });
