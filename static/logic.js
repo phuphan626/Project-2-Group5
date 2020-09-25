2599e8c5be9ffe8912f76864e2be3bdddf38727c
phanphu626 <phanphu626@gmail.com>
2020-09-24 21:41:17 -0400

    Updated pro

--- static/logic.js
+++ static/logic.js
@@ -1,20 +1,15 @@
 // Read the json file in for pitching table
-d3.json('/static/pitching.json').then(data => console.log(data))
-
-
-fetch('/static/pitching.json')
-    .then(data => console.log(data))
-    //   .then(function(users) {
-    //     var data = {
-    //       labels: users.map(function(user) {
-    //         return user.name;
-    //       }),
-    //       series: users.map(function(user) {
-    //         return user.name.length;
-    //       })
-    //     };
-
-//     var chart = new Chartist.Bar('#chart', data, {
-//       distributeSeries: true
-//     });
-//   });
+d3.json('/static/pitching.json').then(function(users) {
+    var data = {
+        Years: users.map(function(user) {
+            return user.Year;
+        }),
+        ERA: users.map(function(user) {
+            return user.ERA;
+        })
+    };
+    console.log(data)
+    var chart = new Chartist.Bar('#chart', data, {
+        distributeSeries: true
+    });
+});
